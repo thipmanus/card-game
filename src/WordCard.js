9b5297513d15be366ad040aced6a950feaf84a58
thipmanus <5910110148@psu.ac.th>
2018-09-17 11:46:10 +0700

    Components & States

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
-export default class WordCard extends
-Component {
+export default class WordCard extends Component {
+activationHandler = c => { console.log(`${c} has been activated.`) }
 render() {
  return (
  <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} 
+    activationHandler={this.activationHandler}/>) }
  </div>
  );
  }
