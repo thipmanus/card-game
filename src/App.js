f7b5e3f0b6db8ff5392ef3e61dab0fc985818bce
thipmanus <5910110148@psu.ac.th>
2018-09-17 11:29:20 +0700

    Components & Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+//import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
+const word = "Hello";
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
- }
+  render() {
+    return (
+    <div>
+    <WordCard value="hello"/>
+    </div>
+    );
+   }
 }
+
 export default App;
---
+++ src/CharacterCard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+export default class CharacterCard extends Component {
+    render(){
+    return (
+    <div className = "card">{this.props.value}</div>
+    )
+    }
+}
+   
